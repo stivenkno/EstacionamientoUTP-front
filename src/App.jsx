62c5e9d551ffe04a4365b680dbf508ca6ca69509
stivenkno <stivencano931@gmail.com>
2025-01-03 19:26:42 -0500

    bug arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
